ffa70db3c254dd9f4ef8a1e48e633abd38a98ed9
Adan <jonesadan@yahoo.com>
2024-03-21 18:32:33 -0400

    First commit of working add card feature

---
+++ src/Layout/AddCard.js
@@ -0,0 +1,94 @@
+import React, { useEffect, useState, useRef } from "react";
+import { Link, useParams, useHistory } from "react-router-dom";
+import { readDeck, createCard } from "../utils/api";
+import CardForm from "./CardForm";
+
+function AddCard() {
+  const mountedRef = useRef(false);
+  const initialFormState = {
+    id: "",
+    front: "",
+    back: "",
+    deckId: "",
+  };
+  const [deck, setDeck] = useState({
+    name: "loading...",
+    description: "",
+    cards: [],
+  });
+  const [newCardData, setNewCardData] = useState(initialFormState);
+  const history = useHistory();
+  const { deckId } = useParams();
+
+  useEffect(() => {
+    mountedRef.current = true;
+    return () => {
+      mountedRef.current =false;
+    };
+  }, []);
+
+  useEffect(() => {
+    const abortController = new AbortController();
+    async function loadDeck() {
+      try {
+        const loadedDeck = await readDeck(deckId, abortController.signal);
+        if (mountedRef.current) {
+          setDeck(() => loadedDeck);
+        }
+      } catch (error) {
+        if (error.name !== "AbortError") {
+          throw error;
+        }
+      }
+    }
+    loadDeck();
+    return () => {
+      abortController.abort();
+    };
+  }, [deckId]);
+
+  const changeHandler = ({ target }) => {
+    setNewCardData((currentState) => ({
+      ...currentState,
+      [target.name]: target.value,
+    }));
+  };
+
+  const submitHandler = async (event) => {
+    event.preventDefault();
+    await createCard(deckId, newCardData);
+    setNewCardData(initialFormState);
+    history.go(0);
+  };
+
+  return (
+    <>
+      <nav aria-label="breadcrumb">
+        <ol className="breadcrumb">
+          <li className="breadcrumb-item">
+            <Link to="/">
+              <i className="fas fa-home"></i> Home
+            </Link>
+          </li>
+          <li className="breadcrumb-item">
+            <Link to={`/decks/${deckId}`}>{deck.name}</Link>
+          </li>
+          <li className="breadcrumb-item active" aria-current="page">
+            Edit Deck
+          </li>
+        </ol>
+      </nav>
+      <h1 className="my-4 text-center">
+        {deck.name}: <span>Add Card</span>
+      </h1>
+      <CardForm
+        changeHandler={changeHandler}
+        submitHandler={submitHandler}
+        newCardData={newCardData}
+        deckId={deckId}
+      />  
+    </>
+  );
+}
+
+export default AddCard;
